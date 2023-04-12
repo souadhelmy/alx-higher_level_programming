ed4445fcc04ad35cb816d92e144d9dcebd7d909b
souadhelmy <soadhamy98@gmail.com>
2023-04-12 16:29:57 -0700

    task2

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -4,7 +4,10 @@
 
 def append_write(filename="", text=""):
         """Appends a string to the end of a UTF8 text file.
-
+           
+        Args:
+        filename (str): The name of the file to append to.
+        text (str): The string to append to the file.
         Returns:
         The number of characters appended.
         """"
