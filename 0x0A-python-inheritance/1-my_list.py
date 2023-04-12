479ad01c164a5fcb570e56b4342b06af7d473429
souadhelmy <soadhamy98@gmail.com>
2023-04-11 21:54:48 -0700

    last1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,23 +1,11 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
 """This module inherits from mylist class"""
-=======
-"""
-Defines an inherited list class MyList.
-"""
->>>>>>> 77216a68444ff374323b33c6bc7f23a5b0d5281a
 
 
 class MyList(list):
     """A subclass of list"""
 
-<<<<<<< HEAD
 def print_sorted(self):
     """prints the sorted list"""
     print_sorted(self):
 
-=======
-    def print_sorted(self):
-        """Print a list in sorted ascending order."""
-        print(sorted(self)):
->>>>>>> 77216a68444ff374323b33c6bc7f23a5b0d5281a
