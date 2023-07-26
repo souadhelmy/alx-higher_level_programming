6d6b741dd5a2fd8bb68453ae768c5b85808f8ebb
souadhelmy <soadhamy98@gmail.com>
2023-07-25 18:44:48 -0700

    all

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,16 +1,31 @@
 #!/usr/bin/node
+
 const request = require('request');
-request(process.argv[2], function (error, response, body) {
-  if (!error) {
-    const todos = JSON.parse(body);
-    const completed = {};
-    todos.forEach((todo) => {
-      if (todo.completed && completed[todo.userId] === undefined) {
-        completed[todo.userId] = 1;
-      } else if (todo.completed) {
-        completed[todo.userId] += 1;
-      }
-    });
-    console.log(completed);
-  }
-});
+
+function countCompletedTasks(apiURL) {
+  request(apiURL, (error, response, body) => {
+    if (error) {
+      console.error('Error:', error);
+    } else if (response.statusCode !== 200) {
+      console.error('Status Code:', response.statusCode);
+    } else {
+      const todos = JSON.parse(body);
+      const completedTasks = {};
+
+      todos.forEach((todo) => {
+        if (todo.completed) {
+          if (!completedTasks[todo.userId]) {
+            completedTasks[todo.userId] = 1;
+          } else {
+            completedTasks[todo.userId]++;
+          }
+        }
+      });
+
+      console.log(completedTasks);
+    }
+  });
+}
+
+const apiURL = process.argv[2];
+countCompletedTasks(apiURL);
